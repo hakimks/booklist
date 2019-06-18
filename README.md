8ac1dff7194db27143ec381baf9a047735869231
hakimks <hakimks@gmail.com>
2019-06-18 13:52:19 +0300

    feat: Get jason data from the googles books APi and display it in a text view

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 How to make your Android app connect to a web service and retrieve JSON data from the web, show it with a RecyclerView and persist data with SharedPreferences. You'll also create menus dynamically and use data binding.
+
+From the google books Api - https://www.googleapis.com/books/v1/volumes/?q=android
--- app/src/main/java/com/hakim/booklist/MainActivity.java
+++ app/src/main/java/com/hakim/booklist/MainActivity.java
@@ -1,13 +1,72 @@
 package com.hakim.booklist;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar mLoadingProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        mLoadingProgress = (ProgressBar) findViewById(R.id.bp_loading);
+
+        try{
+            URL url = ApiUtil.buildUrl("cooking");
+            new BooksQueryTask().execute(url);
+
+        } catch (Exception e){
+            Log.d("Error", e.getMessage());
+        }
+    }
+
+    public class BooksQueryTask extends AsyncTask<URL, Void, String >{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String result = null;
+            try{
+                result = ApiUtil.getJson(searchUrl);
+            } catch (IOException e){
+                Log.d("Error", e.getMessage());
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String jsonResults) {
+            TextView tvResult = (TextView) findViewById(R.id.tvResponse);
+            TextView errorMsg = (TextView) findViewById(R.id.tv_error);
+
+            mLoadingProgress.setVisibility(View.INVISIBLE);
+            if (jsonResults == null){
+                tvResult.setVisibility(View.INVISIBLE);
+                errorMsg.setVisibility(View.VISIBLE);
+
+            } else {
+                tvResult.setVisibility(View.VISIBLE);
+                errorMsg.setVisibility(View.INVISIBLE);
+            }
+
+            tvResult.setText(jsonResults);
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgress.setVisibility(View.VISIBLE);
+        }
     }
 }
