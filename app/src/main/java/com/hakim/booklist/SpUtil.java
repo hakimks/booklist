ee9fff23db3c3e52c6a249f67d854a8dcaefaacd
hakimks <hakimks@gmail.com>
2019-07-29 12:03:23 +0300

    created a shared preference utility class

---
+++ app/src/main/java/com/hakim/booklist/SpUtil.java
@@ -0,0 +1,35 @@
+package com.hakim.booklist;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SpUtil {
+    private SpUtil(){}
+
+    public static final String PREF_NAME = "BooksPrefferences";
+
+    public static SharedPreferences getPref(Context context){
+        return context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public static String getPreferenceString(Context context, String key){
+        return getPref(context).getString(key, "");
+    }
+
+    public static int getPreferenceInt(Context context, String key){
+        return getPref(context).getInt(key, 0);
+    }
+
+    public static void setPreferenceString(Context context, String key, String value){
+        SharedPreferences.Editor editor = getPref(context).edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static void setPreferenceInt(Context context, String key, int value){
+        SharedPreferences.Editor editor = getPref(context).edit();
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+}
